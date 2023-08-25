964b693f7aaaed3a81672a91e6d04e4a929eaa2c
Alberto Marquez <78459098+AlbertoMarquez794@users.noreply.github.com>
2023-08-24 20:36:04 -0600

    Update P1A3-Sem_foro.ino

--- P_Mecatronica/Practica01/P1A3-Sem_foro/P1A3-Sem_foro.ino
+++ P_Mecatronica/Practica01/P1A3-Sem_foro/P1A3-Sem_foro.ino
@@ -6,7 +6,8 @@
 #define LEDVS2 15
 
 void setup() {
-  pinMode (LEDRS1, OUTPUT);
+  //Para todo este bloque asignamos que nuestros leds van a ser salidad. 
+  pinMode (LEDRS1, OUTPUT); 
   pinMode (LEDAS1, OUTPUT);
   pinMode (LEDVS1, OUTPUT);
   pinMode (LEDRS2, OUTPUT);
@@ -16,28 +17,29 @@ void setup() {
 }
 
 void loop() {
+   //Prendemos el led rojo de nuestro semaforo 1 y prendemos el led verde del semaforo 2
    digitalWrite(LEDRS1, HIGH);
    digitalWrite(LEDVS2, HIGH);
-   delay(5000);
-   digitalWrite(LEDRS1, LOW);
-   digitalWrite(LEDVS2, LOW);
-
+   delay(5000); //Tiempo de espera del semaforo 2
+   digitalWrite(LEDRS1, LOW); //Apagamos los leds que ya no utilizaremos
+   digitalWrite(LEDVS2, LOW); 
+   //Cambio de luces para el semaforo 2, y luces rojas para el semaforo 1
    digitalWrite(LEDRS1, HIGH);
    digitalWrite(LEDAS2, HIGH);
    delay(1000);
-   digitalWrite(LEDRS1, LOW);
+   digitalWrite(LEDRS1, LOW);//Apagamos los leds que ya no utilizaremos
    digitalWrite(LEDAS2, LOW);
-   
+   //Cambio de luces para el semaforo 2 en verde, y luces rojas para el semaforo 1
    digitalWrite(LEDVS1, HIGH);
    digitalWrite(LEDRS2, HIGH);
    delay(5000);
-   digitalWrite(LEDVS1, LOW);
+   digitalWrite(LEDVS1, LOW);//Apagamos los leds que ya no utilizaremos
    digitalWrite(LEDRS2, LOW);
-   
+   //Cambio de luces para el semaforo 1, y luces rojas para el semaforo 2
    digitalWrite(LEDAS1, HIGH);
    digitalWrite(LEDRS2, HIGH);
    delay(1000);   
-   digitalWrite(LEDAS1, LOW);
+   digitalWrite(LEDAS1, LOW);//Apagamos los leds que ya no utilizaremos
    digitalWrite(LEDRS2, LOW);
    
    }
