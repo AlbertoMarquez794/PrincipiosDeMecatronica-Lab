3292a1aeb589f863bcae438384135ce2966b117a
AlbertoMarquez794 <thebrayanxd45@gmail.com>
2023-10-05 13:35:34 -0600

    Updating Files

--- P_Mecatronica/Practica05/P5A1-Pot_SentidoMotor/P5A1-Pot_SentidoMotor.ino
+++ P_Mecatronica/Practica05/P5A1-Pot_SentidoMotor/P5A1-Pot_SentidoMotor.ino
@@ -1,55 +1,46 @@
-#define EnM 13
-#define MA 14
-#define MB 12
-#define potenciometro  27
+#define EnM 13           // Define el pin 13 como EnM
+#define MA 14            // Define el pin 14 como MA
+#define MB 12            // Define el pin 12 como MB
+#define potenciometro 27 // Define el pin 27 como potenciometro
 
-
-
-
-int channel=0;
-int freq=1000;
-int resolution=12;
-
+int channel = 0;        // Define una variable llamada "channel" y la inicializa en 0
+int freq = 1000;        // Define una variable llamada "freq" y la inicializa en 1000
+int resolution = 12;    // Define una variable llamada "resolution" y la inicializa en 12
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(EnM,OUTPUT);
-pinMode(MA,OUTPUT);
-pinMode(MB,OUTPUT);
-//para el pwm
-ledcSetup(channel,freq,resolution);
-ledcAttachPin(EnM,channel);
-Serial.begin(115200);
+  pinMode(EnM, OUTPUT);  // Configura el pin EnM como salida
+  pinMode(MA, OUTPUT);   // Configura el pin MA como salida
+  pinMode(MB, OUTPUT);   // Configura el pin MB como salida
+
+  // Configuración para el PWM
+  ledcSetup(channel, freq, resolution); // Configura el canal PWM
+  ledcAttachPin(EnM, channel);          // Asocia el pin EnM al canal PWM
+  Serial.begin(115200);                 // Inicializa la comunicación serial a 115200 baudios
 }
 
+void loop() {
+  float val = analogRead(potenciometro);    // Lee el valor analógico del pin potenciometro y lo almacena en val
+  float dirMotor = ((val * 3.3) / 4095);     // Convierte el valor analógico a voltaje y lo almacena en dirMotor
+  Serial.println(val + String(" bits"));     // Imprime el valor leído del potenciómetro en bits
+  Serial.println(dirMotor + String(" V"));   // Imprime el valor convertido a voltaje
 
-void loop()
- {
-  float val = analogRead(potenciometro);
-  float dirMotor=((val * 3.3) / 4095);
-  Serial.println(val+String(" bits"));
-  Serial.println(dirMotor+ String(" V"));
-
-
-  if(dirMotor>=0 && dirMotor<1.32)
-  {
-    ledcWrite(channel,3000);
-    digitalWrite(MA,LOW);
-    digitalWrite(MB,HIGH);
-    Serial.println("levogiro");
-    delay(500);
-  }else if(dirMotor>=1.32 && dirMotor<1.98)
-  {
-    ledcWrite(channel,0000);
-    digitalWrite(MA,HIGH);
-    digitalWrite(MB,LOW);
-    Serial.println("detenido");
-    delay(500);
-  }else if(dirMotor>=1.98)
-  {  
-    ledcWrite(channel,3000);
-    digitalWrite(MA,HIGH);
-    digitalWrite(MB,LOW);
-    Serial.println("Dextrogiro");
-    delay(500);
+  if (dirMotor >= 0 && dirMotor < 1.32) {
+    ledcWrite(channel, 3000);               // Establece el valor PWM en 3000
+    digitalWrite(MA, LOW);                 // Establece el pin MA en bajo
+    digitalWrite(MB, HIGH);                // Establece el pin MB en alto
+    Serial.println("levogiro");             // Imprime "levogiro"
+    delay(500);                             // Espera 500 milisegundos
+  } else if (dirMotor >= 1.32 && dirMotor < 1.98) {
+    ledcWrite(channel, 0);                  // Apaga el PWM
+    digitalWrite(MA, HIGH);                // Establece el pin MA en alto
+    digitalWrite(MB, LOW);                 // Establece el pin MB en bajo
+    Serial.println("detenido");             // Imprime "detenido"
+    delay(500);                             // Espera 500 milisegundos
+  } else if (dirMotor >= 1.98) {
+    ledcWrite(channel, 3000);               // Establece el valor PWM en 3000
+    digitalWrite(MA, HIGH);                // Establece el pin MA en alto
+    digitalWrite(MB, LOW);                 // Establece el pin MB en bajo
+    Serial.println("Dextrogiro");           // Imprime "Dextrogiro"
+    delay(500);                             // Espera 500 milisegundos
   }
+}
--- P_Mecatronica/Practica05/P5A2-Pot_VelocidadMotor/P5A2-Pot_VelocidadMotor.ino
+++ P_Mecatronica/Practica05/P5A2-Pot_VelocidadMotor/P5A2-Pot_VelocidadMotor.ino
@@ -1,61 +1,56 @@
-#define EnM 13
-#define MA 14
-#define MB 12
-#define potenciometro  27
+// Definición de pines
+#define EnM 13           // Define el pin 13 como EnM
+#define MA 14            // Define el pin 14 como MA
+#define MB 12            // Define el pin 12 como MB
+#define potenciometro 27 // Define el pin 27 como potenciometro
 
-
-int channel=0;
-int freq=1000;
-int resolution=12;
-
+// Variables para configuración PWM
+int channel = 0;        // Define el canal PWM como 0
+int freq = 1000;        // Frecuencia del PWM
+int resolution = 12;    // Resolución del PWM
 
 void setup() {
-pinMode(EnM,OUTPUT);
-pinMode(MA,OUTPUT);
-pinMode(MB,OUTPUT);
-//para el pwm
-ledcSetup(channel,freq,resolution);
-ledcAttachPin(EnM,channel);
-Serial.begin(115200);
+  pinMode(EnM, OUTPUT);  // Configura el pin EnM como salida
+  pinMode(MA, OUTPUT);   // Configura el pin MA como salida
+  pinMode(MB, OUTPUT);   // Configura el pin MB como salida
+
+  // Configuración para el PWM
+  ledcSetup(channel, freq, resolution); // Configura el canal PWM
+  ledcAttachPin(EnM, channel);          // Asocia el pin EnM al canal PWM
+  Serial.begin(115200);                 // Inicializa la comunicación serial a 115200 baudios
 }
 
+void loop() {
+  float val = analogRead(potenciometro);       // Lee el valor analógico del pin potenciometro y lo almacena en val
+  float dirMotor = ((val * 3.3) / 4095);       // Convierte el valor analógico a voltaje y lo almacena en dirMotor
+  float dirMotorAumentada = dirMotor * 100;    // Escala el valor de dirMotor a un rango de 0 a 100
 
-void loop()
- {
-  float val = analogRead(potenciometro);
-  float dirMotor=((val * 3.3) / 4095);
-  //Serial.println(val+String(" bits"));
-  float dirMotorAumentada=dirMotor*100;
-  if(dirMotor>=0 && dirMotor<1.32)
-  {
-    int min=map(dirMotorAumentada,0,132,4095,0);
-    // Serial.println(min + String(" min")); imprime en bits lo que se le manda al motor
-    ledcWrite(channel,min);
-    digitalWrite(MA,LOW);
-    digitalWrite(MB,HIGH);
-    Serial.println("Estado: levogiro");
+  // Comprobación del estado del motor en función del valor del potenciómetro
+  if (dirMotor >= 0 && dirMotor < 1.32) {
+    int min = map(dirMotorAumentada, 0, 132, 4095, 0); // Mapea el valor a un rango PWM inverso
+    ledcWrite(channel, min);            // Establece el valor PWM
+    digitalWrite(MA, LOW);              // Configura el pin MA en bajo
+    digitalWrite(MB, HIGH);             // Configura el pin MB en alto
+    Serial.println("Estado: levogiro");  // Imprime el estado "levogiro"
     delay(500);
-    float porcentaje=map(dirMotorAumentada,0,132,100,0);
-    Serial.println(porcentaje+ String(" %"));
-  }else if(dirMotor>=1.32 && dirMotor<1.98)
-  {
-    ledcWrite(channel,0000);
-    digitalWrite(MA,HIGH);
-    digitalWrite(MB,LOW);
-    Serial.println("Estado: detenido");
+    float porcentaje = map(dirMotorAumentada, 0, 132, 100, 0); // Calcula el porcentaje correspondiente
+    Serial.println(porcentaje + String(" %")); // Imprime el porcentaje
+  } else if (dirMotor >= 1.32 && dirMotor < 1.98) {
+    ledcWrite(channel, 0);               // Apaga el PWM
+    digitalWrite(MA, HIGH);             // Configura el pin MA en alto
+    digitalWrite(MB, LOW);              // Configura el pin MB en bajo
+    Serial.println("Estado: detenido");  // Imprime el estado "detenido"
     delay(500);
     Serial.println("0 %");
-  }else if(dirMotor>=1.98)
-  {  
-    int max=map(dirMotorAumentada,198,330,0,4095);
-   // Serial.println(max+ String(" max")); imprime en bits lo que se le manda al motor
-    ledcWrite(channel,max);
-    digitalWrite(MA,HIGH);
-    digitalWrite(MB,LOW);
-    Serial.println("Estado: Dextrogiro");
+  } else if (dirMotor >= 1.98) {
+    int max = map(dirMotorAumentada, 198, 330, 0, 4095); // Mapea el valor a un rango PWM
+    ledcWrite(channel, max);             // Establece el valor PWM
+    digitalWrite(MA, HIGH);             // Configura el pin MA en alto
+    digitalWrite(MB, LOW);              // Configura el pin MB en bajo
+    Serial.println("Estado: Dextrogiro"); // Imprime el estado "Dextrogiro"
     delay(500);
-    float porcentaje1=map(dirMotorAumentada,198,330,0,100);
-    Serial.println(porcentaje1+ String(" %"));
+    float porcentaje1 = map(dirMotorAumentada, 198, 330, 0, 100); // Calcula el porcentaje correspondiente
+    Serial.println(porcentaje1 + String(" %")); // Imprime el porcentaje
   }
-  Serial.println(dirMotor+ String(" V"));
+  Serial.println(dirMotor + String(" V")); // Imprime el voltaje actual del potenciómetro
 }
