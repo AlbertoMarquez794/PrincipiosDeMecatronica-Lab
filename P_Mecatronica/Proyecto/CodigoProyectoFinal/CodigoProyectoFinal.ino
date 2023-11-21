415bace8d1aa2ad3b6e668c672acdd92a3e84d83
AlbertoMarquez794 <thebrayanxd45@gmail.com>
2023-11-21 15:34:13 -0600

    Actualizacion Sensores

--- P_Mecatronica/Proyecto/CodigoProyectoFinal/CodigoProyectoFinal.ino
+++ P_Mecatronica/Proyecto/CodigoProyectoFinal/CodigoProyectoFinal.ino
@@ -9,23 +9,39 @@
 //LCD
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+/*-------------------------------*/
+//Fotoresistores
+#define fotoIzq 34 
+#define fotoDer 35 
 /*--------------------------------*/
 //Infrarrojo
 #define infroj3 18 
 #define infroj1 5 
 #define infroj2 17
 #define infroj4 19
+/*--------------------------------*/
+//Ultrasonico
+#define echoPin 12
+#define trigPin 13
+/*--------------------------------*/
 
+int channel = 0;
+int freq = 1000;
+int resolution = 8;
 
+int channel2 = 0;
+int freq2 = 1000;
+int resolution2 = 8;
 
+/*--------------------------------*/
+volatile long pulses = 0; //Ponemos los pulsos en cero
 
 /*¨*/
-
 #include "BluetoothSerial.h"
 BluetoothSerial SerialBT;
 char dato;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-volatile long pulses = 0; //Ponemos los pulsos en cero
+//volatile long pulses = 0; //Ponemos los pulsos en cero
 
 void setup() { //Declara las variables del puente H como outputs y las del encoder como inputs
   pinMode(IN3,OUTPUT);
@@ -41,6 +57,24 @@ void setup() { //Declara las variables del puente H como outputs y las del encod
   pinMode (infroj3, INPUT);
   pinMode (infroj4, INPUT);
 
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+
+  //Señal pwm motor izquierdo
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(ENA, channel);
+  //Señal pwm motro derecha
+  ledcSetup(channel2, freq2, resolution2);
+  ledcAttachPin(ENB, channel2);
+
+
+  /*Configuración del fotoresistor*/
+
+  pinMode(fotoIzq, INPUT); 
+  pinMode(fotoDer, INPUT); 
+  /*Inicialización de los parametros*/
   Serial.begin(115200); //Inicializamos el serial en 115200 baudios
   SerialBT.begin("Equipo1ESP");
   lcd.init();
@@ -48,13 +82,16 @@ void setup() { //Declara las variables del puente H como outputs y las del encod
 }
 
 void loop() {
-  obstaculos(3000);
+     movimientoAdelante(5000);
+     parar();
+     movimientoDerecha(5000);
 }
+
 
 void parar(){
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
-     digitalWrite(IN3, LOW);
+    digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
     digitalWrite(ENA,LOW);
     digitalWrite(ENB, LOW);
@@ -64,6 +101,7 @@ void parar(){
 }
 
 void movimientoAdelante(int delayM){
+  
     //Motor derecho asociados EL PIN 16 y 4
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
@@ -74,11 +112,11 @@ void movimientoAdelante(int delayM){
     digitalWrite(ENB, HIGH);
     lcd.setCursor(0,0);
     lcd.print("Adelante");
+    ledcWrite(channel, 125);
+    ledcWrite(channel2, 145);
     // Configurar velocidad del motor A mediante PWM
-    analogWrite(ENA, 127);  // Ajusta el valor (0-255) para controlar la velocidad
+    
 
-    // Configurar velocidad del motor B mediante PWM
-    analogWrite(ENB, 127);  // Ajusta el valor (0-255) para controlar la velocidad
     delay(delayM);
 }
 void movimientoReversa(int delayM){
@@ -92,11 +130,9 @@ void movimientoReversa(int delayM){
     digitalWrite(ENB, HIGH);
     lcd.setCursor(0,0);
     lcd.print("Reversa");
-    // Configurar velocidad del motor A mediante PWM
-    analogWrite(ENA, 127);  // Ajusta el valor (0-255) para controlar la velocidad
-
-    // Configurar velocidad del motor B mediante PWM
-    analogWrite(ENB, 127);  // Ajusta el valor (0-255) para controlar la velocidad
+    //Señal pwm motor izquierdo
+    ledcWrite(channel, 125);
+    ledcWrite(channel2, 125);
     delay(delayM);
 }
 
@@ -112,10 +148,9 @@ void movimientoDerecha(int delayM){
     lcd.setCursor(0,0);
     lcd.print("Derecha");
     // Configurar velocidad del motor A mediante PWM
-    analogWrite(ENA, 127);  // Ajusta el valor (0-255) para controlar la velocidad
-
-    // Configurar velocidad del motor B mediante PWM
-    analogWrite(ENB, 127);  // Ajusta el valor (0-255) para controlar la velocidad
+    //Señal pwm motor izquierdo
+    ledcWrite(channel, 125);
+    ledcWrite(channel2, 145);
     delay(delayM);
 }
 
@@ -130,22 +165,53 @@ void movimientoIzquierda(int delayM){
     digitalWrite(ENB, LOW);
     lcd.setCursor(0,0);
     lcd.print("Izquierda");
-    // Configurar velocidad del motor A mediante PWM
-    analogWrite(ENA, 127);  // Ajusta el valor (0-255) para controlar la velocidad
-
-    // Configurar velocidad del motor B mediante PWM
-    analogWrite(ENB, 127);  // Ajusta el valor (0-255) para controlar la velocidad
+   //Señal pwm motor izquierdo
+    ledcWrite(channel, 125);
+    ledcWrite(channel2, 125);
     delay(delayM);
 }
 
+void ultrasonico(int delayUlt){
+   float duracionUlt, distanciaUlt;
+   digitalWrite(trigPin, LOW);
+   delayMicroseconds(1000);
+
+   digitalWrite(trigPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(trigPin, LOW);
+
+  duracionUlt = pulseIn(echoPin,HIGH);
+
+  distanciaUlt = 0.0343 * duracionUlt * 0.5;
+
+  //lcd.setCursor(11,0); 
+  //lcd.print("L:"+String(distanciaUltrasonico));
+  Serial.println("Distancia:"+String(distanciaUlt)+" | ");
+  delay(3000);
+}
+
+void fotoresistor(int delayFoto){
+  int luzIzq = analogRead(fotoIzq);
+  int luzDer = analogRead(fotoDer);
+  lcd.setCursor(0,1);
+  lcd.print("I:" + String(luzIzq) + " | D:" + String(luzDer));
+  
+
+  Serial.println("LDR_Izq:"+String(luzIzq)+" | LDR_Der:"+String(luzDer)+" |");
+  delay(delayFoto);
+}
+
 void obstaculos(int delayInf){
-    int inf3 = digitalRead(infroj3);
+    int inf3 = digitalRead(infroj3); 
     int inf1 = digitalRead(infroj1);
     int inf2 = digitalRead(infroj2);
     int inf4 = digitalRead(infroj4);
     bool  arrInf[] = {inf1, inf2, inf3, inf4};
     int flagsObstaculos [] = {0,0,0,0};
-
+    //inf[3] es el primer de izquierda a derecha
+    //inf[2] es el segundo de izquierda a derecha
+    //inf[1] es el tercero de izquierda a derecha
+    //inf[0] es el cuarto de izquierda a derecha
     for (int i=0; i < 4; i ++){
      if (!arrInf[i]){
         flagsObstaculos[i] = 1; 
@@ -154,8 +220,6 @@ void obstaculos(int delayInf){
 
     Serial.println("Obst 1:"+String(flagsObstaculos[0])+" "+ " Obst2: " + String(flagsObstaculos[1]) + " Obs3: " + String(flagsObstaculos[2]) + " Obst4: " +String(flagsObstaculos[3])+" | ");
     lcd.setCursor(0,0);   // Establece la posición del cursor en la columna 0, fila 0
-    lcd.print(String(flagsObstaculos[0])+" "+String(flagsObstaculos[1]));
-    lcd.setCursor(0,1); 
-    lcd.print(String(flagsObstaculos[2])+" "+String(flagsObstaculos[3]));
+    lcd.print(String(flagsObstaculos[3])+" "+String(flagsObstaculos[2])+ " " + String(flagsObstaculos[1]) + " " + String(flagsObstaculos[0]) + " | ");
     delay(delayInf); 
   }
